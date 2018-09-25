9b195b6737f6da484c456fe903f8bfd8a5008c3a
sjwilczynski <sjwilczynski@gmail.com>
2018-09-25 23:29:19 +0200

    update command to run

--- data_analysis.R
+++ data_analysis.R
@@ -1,6 +1,6 @@
 # IMPORTANT: Run simulations using screen. The instruction is in README.md.
 
-# Usage example: Rscript script_varclust.R 8 28 30 30 "c(10,20,30,40)" "valerie_data.txt"
+# Usage example: Rscript script_varclust.R 8 28 40 30 "c(50,100,150,200,75,125,175,225,250)" "valerie_data.txt"
 library(varclust)
 
 args        <- commandArgs(trailingOnly = TRUE)
@@ -12,6 +12,7 @@ clusts      <- eval(parse(text=args[5])) #number of clusters to test
 filename    <- args[6] #name of the file in which data is stored
 
 
+set.seed(42)
 x <- t(read.table(filename, header = T))
 for(num_clust in clusts){
   print(paste("Starting mlcc.reps for", num_clust, "clusters", sep = " "))
